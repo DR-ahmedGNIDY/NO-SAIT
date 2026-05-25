82cc8830bb5170c4abde058a045e9a271ff8b393
DR-ahmedGNIDY <ahmedahmed.73555@gmail.com>
2026-05-25 17:32:11 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,39 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { verifyToken } from './lib/auth'
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('token')?.value
-  const { pathname } = request.nextUrl
-
-  // حماية الداشبورد
-  if (pathname.startsWith('/admin/dashboard')) {
-    if (!token) {
-      return NextResponse.redirect(new URL('/admin/login', request.url))
-    }
-
-    try {
-      const user: any = verifyToken(token)
-
-      if (!user || user.role !== 'admin') {
-        return NextResponse.redirect(new URL('/admin/login', request.url))
-      }
-    } catch (error) {
-      return NextResponse.redirect(new URL('/admin/login', request.url))
-    }
-  }
-
-  // منع دخول صفحة اللوجين لو الأدمن مسجل بالفعل
-  if (pathname === '/admin/login' && token) {
-    try {
-      const user: any = verifyToken(token)
-
-      if (user && user.role === 'admin') {
-        return NextResponse.redirect(new URL('/admin/dashboard', request.url))
-      }
-    } catch (error) {}
-  }
-
   return NextResponse.next()
 }
 
